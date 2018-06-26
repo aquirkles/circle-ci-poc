262bb817a14f813ca9854621a319f56b427575b8
quirkles <al.quirk@gmail.com>
2018-06-26 11:01:47 -0400

    Cause test to fail

--- ui/index.spec.js
+++ ui/index.spec.js
@@ -2,6 +2,6 @@ import app from './index'
 
 describe('ui app', () => {
   it('works as expected', () => {
-    expect(app('hello world')).toEqual("A_MESSAGE_FROM_THE_UI:_HELLO_WORLD")
+    expect(app('hello world')).toEqual("A_MESSAGE_FROM_THE_UI:_HELLO_WORL")
   })
 })
